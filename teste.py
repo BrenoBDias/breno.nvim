f0edb1963c71897ee809243ad05ccb5970e26423
BrenoBDias <99230311+BrenoBDias@users.noreply.github.com>
2024-05-07 21:08:53 -0300

    -

---
+++ teste.py
@@ -0,0 +1,32 @@
+= 2
+b = "3"
+
+
+print(a + b)
+
+
+
+
+a={
+    1,
+    2,
+    3,
+    4,
+    5
+    6,
+    7,
+}
+
+def asdf():
+    if true:
+        pass
+
+
+    else:
+        pass
+
+
+
+    return 0
+
+    
